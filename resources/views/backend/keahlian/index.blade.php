4e5cbec6387cb0258abb218ab4957a7700be7d74
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2024-01-21 03:20:19 +0800

    update buttons

--- resources/views/backend/keahlian/index.blade.php
+++ resources/views/backend/keahlian/index.blade.php
@@ -99,7 +99,36 @@
 
     var tableAhli = $('#tableAhli').DataTable({
         dom: 'Bfrtip',
-        buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
+        "buttons": [{
+               "extend": 'copy',
+               "text": '<i class="fa fa-files-o" style="color: green;"></i>',
+               "titleAttr": 'Copy',
+               "action": newexportaction
+            },
+            {
+               "extend": 'excel',
+               "text": '<i class="fa fa-file-excel-o" style="color: green;"></i>',
+               "titleAttr": 'Excel',
+               "action": newexportaction
+            },
+            {
+               "extend": 'csv',
+               "text": '<i class="fa fa-file-text-o" style="color: green;"></i>',
+               "titleAttr": 'CSV',
+               "action": newexportaction
+            },
+            {
+               "extend": 'pdf',
+               "text": '<i class="fa fa-file-pdf-o" style="color: green;"></i>',
+               "titleAttr": 'PDF',
+               "action": newexportaction
+            },
+            {
+               "extend": 'print',
+               "text": '<i class="fa fa-print" style="color: green;"></i>',
+               "titleAttr": 'Print',
+               "action": newexportaction
+            }],
         "processing": true,
         "serverSide": true,
         "responsive": true,
@@ -123,6 +152,48 @@
           { "data": "tindakan", orderable:false, searchable:false }
         ]
     });
+
+    function newexportaction(e, dt, button, config) {
+    var self = this;
+    var oldStart = dt.settings()[0]._iDisplayStart;
+    dt.one('preXhr', function (e, s, data) {
+        // Just this once, load all data from the server...
+        data.start = 0;
+        data.length = 2147483647;
+        dt.one('preDraw', function (e, settings) {
+            // Call the original action function
+            if (button[0].className.indexOf('buttons-copy') >= 0) {
+                $.fn.dataTable.ext.buttons.copyHtml5.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-excel') >= 0) {
+                $.fn.dataTable.ext.buttons.excelHtml5.available(dt, config) ?
+                    $.fn.dataTable.ext.buttons.excelHtml5.action.call(self, e, dt, button, config) :
+                    $.fn.dataTable.ext.buttons.excelFlash.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-csv') >= 0) {
+                $.fn.dataTable.ext.buttons.csvHtml5.available(dt, config) ?
+                    $.fn.dataTable.ext.buttons.csvHtml5.action.call(self, e, dt, button, config) :
+                    $.fn.dataTable.ext.buttons.csvFlash.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-pdf') >= 0) {
+                $.fn.dataTable.ext.buttons.pdfHtml5.available(dt, config) ?
+                    $.fn.dataTable.ext.buttons.pdfHtml5.action.call(self, e, dt, button, config) :
+                    $.fn.dataTable.ext.buttons.pdfFlash.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-print') >= 0) {
+                $.fn.dataTable.ext.buttons.print.action(e, dt, button, config);
+            }
+            dt.one('preXhr', function (e, s, data) {
+                // DataTables thinks the first item displayed is index 0, but we're not drawing that.
+                // Set the property to what it was before exporting.
+                settings._iDisplayStart = oldStart;
+                data.start = oldStart;
+            });
+            // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
+            setTimeout(dt.ajax.reload, 0);
+            // Prevent rendering of the full data to the DOM
+            return false;
+        });
+    });
+    // Requery the server with the new one-time export settings
+    dt.ajax.reload();
+};
 
     $('#jalan, #status, #jenis').change(function (e) {
         e.preventDefault();
